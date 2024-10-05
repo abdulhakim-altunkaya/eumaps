f824f43dadba741dee6e8888b64d68ee4d54fe0e
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-10-06 00:57:17 +0300

    converters main page initiated

--- client/src/components/ConvertersMain.js
+++ client/src/components/ConvertersMain.js
@@ -3,16 +3,22 @@ import "../styles/ConvertersMain.css";
 
 function ConvertersMain() {
   return (
-    <div className='convertersIconsMainArea'>
-        <div>
-            <span>Weight</span><span><img src="/icons/weight.png" className="unitIcons" alt="Clickable Weight Icon"/></span>
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className='convertersMain'>
+      <h1>CONVERTERS & CALCULATORS</h1>
+      <div className='convertersIconsMainArea'>
+          
+          <div className="converter-item">
+              <span className="converter-name">Weight</span>
+              <span className="icon-wrapper"><img src="/icons/weight.png" className="unitIcons" alt="Clickable Weight Icon"/></span>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
 
+      </div>
     </div>
+
   )
 }
 
