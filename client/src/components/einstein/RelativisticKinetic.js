53ab3b6671f8c57fa5c247b2e1307cd9cdca14d5
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-10-17 14:03:23 +0300

    relativistic kinetic energy converter is finished

--- client/src/components/einstein/RelativisticKinetic.js
+++ client/src/components/einstein/RelativisticKinetic.js
@@ -20,10 +20,10 @@ function RelativisticKinetic() {
     e.preventDefault(); // prevent form from refreshing page
 
     const formData = new FormData(e.target);
-    const restMass = formData.get('restMass'); // in kilograms
+    const restMassGrams = formData.get('restMass'); // in grams
     const velocityKmPerSec = formData.get('velocity'); // in km/s
 
-    let m0 = Number(restMass);
+    let m0 = Number(restMassGrams) / 1000; // Convert grams to kilograms
     let vKm = Number(velocityKmPerSec);
     let v = vKm * 1000; // Convert km/s to m/s
 
@@ -53,19 +53,19 @@ function RelativisticKinetic() {
 
     // Convert the energy into different units
     const energy = {
-      joules: kineticEnergyJoules.toFixed(3),
-      kWh: (kineticEnergyJoules * JOULES_TO_KWH).toFixed(10),
-      eV: (kineticEnergyJoules * JOULES_TO_EV).toExponential(3),
-      calories: (kineticEnergyJoules * JOULES_TO_CAL).toFixed(3),
-      megatonsTNT: (kineticEnergyJoules * JOULES_TO_MEGATONS_TNT).toFixed(10),
-      kgTNT: (kineticEnergyJoules * JOULES_TO_KG_TNT).toFixed(6),
-      hiroshimaBombs: (kineticEnergyJoules / JOULES_TO_HIROSHIMA).toFixed(6),
-      tsarBombs: (kineticEnergyJoules / JOULES_TO_TSAR_BOMB).toFixed(9),
+      joules: kineticEnergyJoules.toFixed(2),
+      kWh: (kineticEnergyJoules * JOULES_TO_KWH).toFixed(2),
+      eV: (kineticEnergyJoules * JOULES_TO_EV).toExponential(2),
+      calories: (kineticEnergyJoules * JOULES_TO_CAL).toFixed(2),
+      megatonsTNT: (kineticEnergyJoules * JOULES_TO_MEGATONS_TNT).toFixed(3),
+      kgTNT: (kineticEnergyJoules * JOULES_TO_KG_TNT).toFixed(3),
+      hiroshimaBombs: (kineticEnergyJoules / JOULES_TO_HIROSHIMA).toFixed(3),
+      tsarBombs: (kineticEnergyJoules / JOULES_TO_TSAR_BOMB).toFixed(5),
     };
 
     setResultArea(
-      <div style={{ textAlign: 'left' }}>
-        <span>Rest Mass: {m0} kilograms</span> <br/>
+      <div className='relativisticKineticMainDiv' style={{ textAlign: 'left' }}>
+        <span>Rest Mass: {restMassGrams} grams</span> <br/>
         <br/>
         <span>Velocity: {vKm} km/s</span> <br/><br/>
         <p><strong>Energy in Joules:</strong> {energy.joules} J</p>
@@ -78,11 +78,34 @@ function RelativisticKinetic() {
         <p><strong>Equivalent in Tsar Hydrogen Bombs:</strong> {energy.tsarBombs} bombs</p>
         <br/>
         <span>
-          Relativistic Kinetic Energy Equation (KE: kinetic energy, m: rest mass, v: velocity km/s, c: speed of light m/s):
+          Relativistic Kinetic Energy Equation (KE: kinetic energy, m: rest mass in grams, y: Lorentz factor, c: speed of light km/s):
         </span>
         <BlockMath>
           {'KE = (\\gamma - 1) m c^2'}
         </BlockMath>
+        <span>
+          In relativity, the total energy of an object is given by equation below. For an object at rest, Lorentz factor is 1.
+          For an object moving at relativistic speeds, Lorentz factor grows very large.
+        </span>
+          <BlockMath>
+            {'E_{total} = \\gamma mc^2'}
+          </BlockMath>
+        <span> 
+          This total energy includes both the rest energy and the energy due to motion (kinetic energy). 
+          To isolate the kinetic energy, we subtract the rest energy from the total energy:
+          <BlockMath>
+            {'KE = E_{total} - E_{rest} = \\gamma mc^2 - mc^2 = (\\gamma - 1) mc^2'}
+          </BlockMath>
+        </span>
+        <span>
+          If an object moving at relativistic speeds hits the Earth, approximately 90% of its kinetic energy will
+          convert into an explosion (heat, shockwaves, seismic waves, etc). Atmospheric drag for objects moving at 
+          relativistic speed is negligible. During these type of impacts, rest enegy of the object is not converted into
+          explosion, only its kinetic energy is converted to explosion (90%). For impact scenarios where kinetic energy 
+          and rest energy together convert into explosion, the impact should be accompanied by a type of nuclear or 
+          matter-antimatter annihilation.
+        </span>
+
       </div>
     );
   }
@@ -103,11 +126,11 @@ function RelativisticKinetic() {
           type='number'
           name='restMass'
           id='restMass'
-          aria-label='Enter Rest Mass in kilograms.'
+          aria-label='Enter Rest Mass in grams.'
           min="1"
           required
         /> &nbsp; &nbsp;
-        <label htmlFor='restMass'>Enter Rest Mass (in kilograms)</label> <br/><br/>
+        <label htmlFor='restMass'>Enter Rest Mass (in grams)</label> <br/><br/>
 
         <input
           className='inputFields'
