760ca27674b38d06fd0066e51279dfc42307ac8e
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-10-20 14:38:29 +0300

    comment display error fix: very simple code was missing, res.json(allresults) from server code, I added it. Hopefully it will be solved now

--- server.js
+++ server.js
@@ -86,16 +86,13 @@ app.post("/serversavecommentreply", async (req, res) => {
 app.get("/servergetcomments/:pageId", async (req, res) => {
   let client;
   const { pageId } = req.params;
-  console.log("page id before server request", pageId);
   try {
     client = await pool.connect(); 
     const result = await client.query(
       `SELECT * FROM eumaps_comments WHERE sectionid = $1 ORDER BY id DESC`, [Number(pageId)]
     );
     const allComments = result.rows;
-    console.log(`hi from server, here are results: ${allComments}`)
-    console.log("page id after server request", pageId);
-
+    res.status(200).json(allComments);
   } catch (error) {
     console.log(error.message);
     res.status(500).json({message: "Error while fetching comments"})
