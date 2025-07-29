bd775f398e79bdcd022c70d2a69eb5c9a2423f0c
abdulhakim-altunkaya <altunkaya2@gmail.com>
2025-07-29 17:34:42 +0300

    converters removed from eumaps

--- client/src/components/MainArea.js
+++ client/src/components/MainArea.js
@@ -1,7 +1,6 @@
-import React from 'react';
+
 import {Routes, Route} from "react-router-dom";
 
-import MainConverters from './MainConverters';
 import MainTools from './MainTools';
 import MainHariciye from './MainHariciye';
 
@@ -13,13 +12,6 @@ import CustomsGasoline from "./hariciye/CustomsGasoline"; //5
 import CustomsHybridPlugin from "./hariciye/CustomsHybridPlugin"; //6
 import CustomsBike from "./hariciye/CustomsBike"; //7
 import CustomsNotes from "./hariciye/CustomsNotes"; //8
-
-import Weight from "./converters/Weight"; //14
-import Length from "./converters/Length"; //15
-import Temperature from "./converters/Temperature"; //16
-import Area from "./converters/Area"; //17
-import Volume from "./converters/Volume"; //18
-import Time from "./converters/Time"; //19
 
 import Investment from "./subcomponents/Investment"; //20
 import Counter from "./wordTools/Counter"; //21
@@ -46,19 +38,11 @@ function MainArea() {
         <Route path="/motorsiklet-gumruk-vergi-hesaplama" element={<CustomsBike/>} />
         <Route path="/bedelsiz-arac-ithalati-onemli-notlar" element={<CustomsNotes/>} />
 
-        <Route path="/weight-units-converter" element={<Weight/>} />
-        <Route path="/length-units-converter" element={<Length/>} />
-        <Route path="/temperature-units-converter" element={<Temperature/>} />
-        <Route path="/area-units-converter" element={<Area/>} />
-        <Route path="/volume-units-converter" element={<Volume/>} />
-        <Route path="/time-units-converter" element={<Time/>} />
-
         <Route path="/character-and-word-counter" element={<Counter/>} />
         <Route path="/word-insulator" element={<WordInsulator/>} />
         <Route path="/sentence-splitter" element={<SentenceSplitter/>} />
 
         <Route path="/free-css-buttons" element={<ButtonsCSS/>} />
-        <Route path="/converters" element={<MainConverters/>} />
         <Route path="/hariciye" element={<MainHariciye/>} />
         <Route path="/tools" element={<MainTools/>} />
         <Route path="/" element={<IndexComp/>} />
