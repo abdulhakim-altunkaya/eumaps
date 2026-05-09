119fc2bb41624c53a22b40c20a7caece7c548a45
abdulhakim-altunkaya <altunkaya2@gmail.com>
2026-05-09 15:48:00 +0300

    deactivation bug fixed

--- routes/grills_LV.js
+++ routes/grills_LV.js
@@ -2679,6 +2679,8 @@ router.get("/api/get/grills-latvia/posting-owner-ads", applyReadRateLimit, async
         is_active       
       FROM grills_lv_ads
       WHERE google_id = $1
+      WHERE google_id = $1
+      AND is_active = true
       ORDER BY date DESC, id DESC;
     `;
     const adsRes = await pool.query(adsQuery, [gid]);
